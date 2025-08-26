ad6226659d5b84b0d01511f728af9c9fa110392c
Edumachadoh <dudu.henrique.em@gmail.com>
2025-08-26 17:59:01 -0300

    Criando botão de navegação e primeira navegação

--- app/src/main/java/com/example/linkedlncurrculos/HomeActivity.kt
+++ app/src/main/java/com/example/linkedlncurrculos/HomeActivity.kt
@@ -1,20 +1,28 @@
 package com.example.linkedlncurrculos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.linkedlncurrculos.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding // conecta o front com o back
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.arrowHomeButtom.setOnClickListener { // criar evento no botão arrow, que quando clicado leva até a main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
